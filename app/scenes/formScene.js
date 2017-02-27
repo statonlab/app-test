504c9a5084c2b48293ab5301a8655303d754b341
Bradford Condon <bcondon@Bradfords-iMac.local>
2017-02-27 15:21:56 -0500

    added dropdown buttons and test alt type of dropdown button

--- app/scenes/formScene.js
+++ app/scenes/formScene.js
@@ -58,7 +58,51 @@ const styles = StyleSheet.create({
     subHeadText : {
         fontSize : 22,
         flex : 1
-    }
+    },
+    dropdown_2: {
+        alignSelf: 'flex-end',
+        width: 150,
+        top: 32,
+        right: 8,
+        borderWidth: 0,
+        borderRadius: 3,
+        backgroundColor: 'cornflowerblue',
+    },
+    dropdown_2_text: {
+        marginVertical: 10,
+        marginHorizontal: 6,
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+    },
+    dropdown_2_dropdown: {
+        width: 150,
+        height: 300,
+        borderColor: 'cornflowerblue',
+        borderWidth: 2,
+        borderRadius: 3,
+    },
+    dropdown_2_row: {
+        flexDirection: 'row',
+        height: 40,
+        alignItems: 'center',
+    },
+    dropdown_2_image: {
+        marginLeft: 4,
+        width: 30,
+        height: 30,
+    },
+    dropdown_2_row_text: {
+        marginHorizontal: 4,
+        fontSize: 16,
+        color: 'navy',
+        textAlignVertical: 'center',
+    },
+    dropdown_2_separator: {
+        height: 1,
+        backgroundColor: 'cornflowerblue',
+    },
 })
 
 const Textfield = MKTextField.textfield()
@@ -103,16 +147,16 @@ const onButtonPress = () => {
 
 const treeDensities = ['0-10/100 sq feet', '11-50/100 sq feet', '>50/100 sq feet'];
 const diseaseCoverage = ['0-25%', '26-50%', '51-75%', '76-100%'];
+const DEMO_OPTIONS_1 = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7', 'option 8', 'option 9'];
 
-
-//https://github.com/xinthink/react-native-material-kit
 
 export default class FormScene extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        }
+    }
 
     render() {
+
         return (
             <View style={{backgroundColor: '#f5f5f5', flex: 1, flexDirection: 'column'}}>
                 <Header title={this.props.title} navigator={this.props.navigator} initial={true}/>
@@ -138,6 +182,15 @@ export default class FormScene extends Component {
                         </View>
                     </View>
                     <View style = {{flexDirection: 'row'}}>
+                        <ModalDropdown style={styles.dropdown_2}
+                                       textStyle={styles.dropdown_2_text}
+                                       dropdownStyle={styles.dropdown_2_dropdown}
+                                       options={treeDensities}
+                                       renderRow={this._dropdown_2_renderRow.bind(this)}
+                                       renderSeparator={(sectionID, rowID, adjacentRowHighlighted) => this._dropdown_2_renderSeparator(sectionID, rowID, adjacentRowHighlighted)}
+                        />
+
+
 
                     </View>
                     <Textfield/>
@@ -147,8 +200,38 @@ export default class FormScene extends Component {
                     (Selected image and/or GPS coordinate below)</Text>
                 </View>
             </View>
-        )
+        );
     }
+
+    _dropdown_2_renderRow(rowData, rowID, highlighted) {
+        let icon = highlighted ? require('../../images/heart.png') : require('../../images/flower.png');
+        let evenRow = rowID % 2;
+        return (
+            <TouchableHighlight underlayColor='cornflowerblue'>
+                <View style={[styles.dropdown_2_row, {backgroundColor: evenRow ? 'lemonchiffon' : 'white'}]}>
+                    <Image style={styles.dropdown_2_image}
+                           mode='stretch'
+                           source={icon}
+                    />
+                    <Text style={[styles.dropdown_2_row_text, highlighted && {color: 'mediumaquamarine'}]}>
+                        {`${rowData}`}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+
+
+    _dropdown_2_renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
+        if (rowID == treeDensities.length - 1) return;
+        let key = `spr_${rowID}`;
+        return (<View style={styles.dropdown_2_separator}
+                      key={key}
+        />);
+    }
+
+
+
 }
 
 FormScene.propTypes = {
@@ -157,5 +240,4 @@ FormScene.propTypes = {
 }
 
 const elevationStyle = new Elevation(2)
-
 
