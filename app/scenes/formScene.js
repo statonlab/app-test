ccfd71b8b619bfdadbc5f9ef6c96b459ed737a97
Bradford Condon <bcondon@Bradfords-iMac.local>
2017-02-27 17:35:12 -0500

    switched ModalPicker

--- app/scenes/formScene.js
+++ app/scenes/formScene.js
@@ -24,8 +24,7 @@ import {
     mdl,
 } from 'react-native-material-kit'
 
-import ModalDropdown from 'react-native-modal-dropdown';
-
+import ModalPicker from 'react-native-modal-picker'
 
 const theme  = getTheme();
 const myIcon = (<Icon name="md-arrow-dropright-circle" size={22} color="#959595"/>);
@@ -59,50 +58,7 @@ const styles = StyleSheet.create({
         fontSize : 22,
         flex : 1
     },
-    dropdown_2: {
-        alignSelf: 'flex-end',
-        width: 150,
-        top: 32,
-        right: 8,
-        borderWidth: 0,
-        borderRadius: 3,
-        backgroundColor: 'cornflowerblue',
-    },
-    dropdown_2_text: {
-        marginVertical: 10,
-        marginHorizontal: 6,
-        fontSize: 18,
-        color: 'white',
-        textAlign: 'center',
-        textAlignVertical: 'center',
-    },
-    dropdown_2_dropdown: {
-        width: 150,
-        height: 300,
-        borderColor: 'cornflowerblue',
-        borderWidth: 2,
-        borderRadius: 3,
-    },
-    dropdown_2_row: {
-        flexDirection: 'row',
-        height: 40,
-        alignItems: 'center',
-    },
-    dropdown_2_image: {
-        marginLeft: 4,
-        width: 30,
-        height: 30,
-    },
-    dropdown_2_row_text: {
-        marginHorizontal: 4,
-        fontSize: 16,
-        color: 'navy',
-        textAlignVertical: 'center',
-    },
-    dropdown_2_separator: {
-        height: 1,
-        backgroundColor: 'cornflowerblue',
-    },
+
 })
 
 const Textfield = MKTextField.textfield()
@@ -145,12 +101,25 @@ const onButtonPress = () => {
 };
 
 
-const treeDensities = ['0-10/100 sq feet', '11-50/100 sq feet', '>50/100 sq feet'];
-const diseaseCoverage = ['0-25%', '26-50%', '51-75%', '76-100%'];
-const DEMO_OPTIONS_1 = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7', 'option 8', 'option 9'];
+
+let index = 0;
+const treeDensities = [
+    { key: index++, label: '0-10/100 sq feet' },
+    { key: index++, label: '11-50/100 sq feet' },
+    { key: index++, label: '51-100/100 sq feet' },
+    { key: index++, label: '>100/100 sq feet' },
+];
+
+//const treeDensities = ['0-10/100 sq feet', '11-50/100 sq feet', '>50/100 sq feet'];
+index = 0;
+const diseaseCoverage = [
+    { key: index++, label: '0-25%'},
+    { key: index++, label:'26-50%'},
+    { key: index++, label: '51-75%' },
+        { key: index++, label: '76-100%'}]
 
 
-export default class FormScene extends Component {
+        export default class FormScene extends Component {
     constructor(props) {
         super(props);
     }
@@ -168,7 +137,10 @@ export default class FormScene extends Component {
                         <View style={{flexGrow:1, padding: 0, height: 50, flexDirection:'row'}}>
                             <Text>
                                 Tree density: </Text>
-                            <ModalDropdown options={treeDensities}/>
+                            <ModalPicker
+                                data={treeDensities}
+                                initValue="Select density of this tree species for this entry."
+                                onChange={(option)=>{ alert(`${option.label} (${option.key}) Updated tree density`) }} />
                         </View>
                     </View>
                     <View
@@ -178,20 +150,13 @@ export default class FormScene extends Component {
                         <View style={{flexGrow:1, height: 50, flexDirection:'row'}}>
                             <Text>
                                 % Disease coverage </Text>
-                            <ModalDropdown options={diseaseCoverage}/>
+                            <ModalPicker
+                                data={diseaseCoverage}
+                                initValue="Select disease coverage for this tree."
+                                onChange={(option)=>{ alert(`${option.label} (${option.key}) Updated disease coverage`) }} />
                         </View>
                     </View>
                     <View style = {{flexDirection: 'row'}}>
-                        <ModalDropdown style={styles.dropdown_2}
-                                       textStyle={styles.dropdown_2_text}
-                                       dropdownStyle={styles.dropdown_2_dropdown}
-                                       options={treeDensities}
-                                       renderRow={this._dropdown_2_renderRow.bind(this)}
-                                       renderSeparator={(sectionID, rowID, adjacentRowHighlighted) => this._dropdown_2_renderSeparator(sectionID, rowID, adjacentRowHighlighted)}
-                        />
-
-
-
                     </View>
                     <Textfield/>
                     <Text></Text>
@@ -202,35 +167,6 @@ export default class FormScene extends Component {
             </View>
         );
     }
-
-    _dropdown_2_renderRow(rowData, rowID, highlighted) {
-        let icon = highlighted ? require('../../images/heart.png') : require('../../images/flower.png');
-        let evenRow = rowID % 2;
-        return (
-            <TouchableHighlight underlayColor='cornflowerblue'>
-                <View style={[styles.dropdown_2_row, {backgroundColor: evenRow ? 'lemonchiffon' : 'white'}]}>
-                    <Image style={styles.dropdown_2_image}
-                           mode='stretch'
-                           source={icon}
-                    />
-                    <Text style={[styles.dropdown_2_row_text, highlighted && {color: 'mediumaquamarine'}]}>
-                        {`${rowData}`}
-                    </Text>
-                </View>
-            </TouchableHighlight>
-        );
-    }
-
-
-    _dropdown_2_renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
-        if (rowID == treeDensities.length - 1) return;
-        let key = `spr_${rowID}`;
-        return (<View style={styles.dropdown_2_separator}
-                      key={key}
-        />);
-    }
-
-
 
 }
 
