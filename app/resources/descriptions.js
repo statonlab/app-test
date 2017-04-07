c5814ef5106a1f085932a6e70b003371f443becf
Bradford Condon <bradford.condon@gmail.com>
2017-04-07 15:22:42 -0400

    styling

--- app/resources/descriptions.js
+++ app/resources/descriptions.js
@@ -8,7 +8,7 @@ export default Plants = {
     descriptionCards: [
       {
         title : "Species",
-        body  : [<Text>American chestnut: <Text style={{fontStyle: "italic"}}>  Castanea dentata </Text></Text>],
+        body  : [<Text>American chestnut: <Text style={{fontStyle: "italic"}}>Castanea dentata </Text></Text>],
         images: {}
       }, {
         title : "Introduction",
@@ -21,7 +21,7 @@ export default Plants = {
       }, {
         images: {},
         title : "Chestnut blight",
-        body  : [<Text>Chestnut blight is caused by the fungus Cryphonectria parasitica. It was introduced to North America in the early 1900s by accidentally importing infected Asian chestnut trees.  Most Asian chestnuts are quite resistant to the fungus but American chestnut proved highly susceptible and by 1940 few mature chestnut trees were left.  ",
+        body  : [<Text>Chestnut blight is caused by the fungus <Text style={{fontStyle: "italic"}}>Cryphonectria parasitica</Text>. It was introduced to North America in the early 1900s by accidentally importing infected Asian chestnut trees.  Most Asian chestnuts are quite resistant to the fungus but American chestnut proved highly susceptible and by 1940 few mature chestnut trees were left.",
           "The fungus that causes chestnut blight grows in the vascular system of the tree, forming a canker and eventually girdling the tree.  Symptoms of chestnut blight include cracked bark, sunken cankers, and fungal structures. </Text>,
           <Text>Several breeding programs exist to develop trees that are resistant to chestnut blight.  Many of these rely of hybridizing American chestnuts with resistant Chinese of Japanese chestnuts and then back crossing to American chestnut.  In addition, there has been success in producing transgenic chestnut trees with genes from wheat that provide resistance. ",
           "Our hope is that this reporting app will let members of the public identify chestnut trees that have some natural resistance to chestnut blight.  We will then collect seeds from these trees and add them to tree breeding programs to try and propagate resistant trees to use in forest restoration.</Text>],
@@ -41,23 +41,23 @@ export default Plants = {
     descriptionCards: [
       {
         title : "Species",
-        body  : ["While there are many different ash species present in North America, the most common include:", "white ash (Fraxinus americana)", "green ash (Fraxinus pennsylvanica)", "black ash (Fraxinus nigra)", "blue ash (Fraxinus quadrangulata)"],
+        body  : [<Text>While there are many different ash species present in North America, the most common include:</Text>,<Text>white ash (<Text style={{fontStyle: "italic"}}>Fraxinus americana</Text>)</Text>, <Text>green ash (<Text style={{fontStyle: "italic"}}>Fraxinus pennsylvanica</Text>)</Text>, <Text>black ash (<Text style={{fontStyle: "italic"}}>Fraxinus nigra</Text>)</Text>, <Text>blue ash (<Text style={{fontStyle: "italic"}}>Fraxinus quadrangulata</Text>)</Text>],
         images: {}
       },
       {title : "Overview",
-        body : ["There are several different species of American ash trees, all of which are susceptible to the deadly emerald ash borer.  Help us find resistant trees by tagging healthy ash trees."],
+        body : [<Text>There are several different species of American ash trees, all of which are susceptible to the deadly emerald ash borer.  Help us find resistant trees by tagging healthy ash trees.</Text>],
         images : {}
       },
       {
         title : "Description",
-        body  : [ "Ash trees share several features that can be used to distinguish them from other tree species.  Ash trees have an opposite branching pattern, where buds are positioned opposite each other on twigs.  Ash trees also have compound leaves.  Compound leaves are made up of many leaflets, each of which looks like a leaf. However leaves and leaflets can be distinguished because buds are only found at the base of the overall leaf, and not each individual leaflet.  Ashes typically have 5-9 leaflets per leaf, although this varies by species.  In addition, mature ash trees have a characteristic diamond pattern to their bark and ash seeds are distinctively shaped. "],
+        body  : [ <Text>Ash trees share several features that can be used to distinguish them from other tree species.  Ash trees have an opposite branching pattern, where buds are positioned opposite each other on twigs.  Ash trees also have compound leaves.  Compound leaves are made up of many leaflets, each of which looks like a leaf. However leaves and leaflets can be distinguished because buds are only found at the base of the overall leaf, and not each individual leaflet.  Ashes typically have 5-9 leaflets per leaf, although this varies by species.  In addition, mature ash trees have a characteristic diamond pattern to their bark and ash seeds are distinctively shaped.</Text>],
         images: {}
       }, {
         title : "Range",
-        body  : ["Ash trees are present throughout Eastern and Midwestern North America.  Different ash species have different distributions within this range."
-        ],
+        body  : [<Text>Ash trees are present throughout Eastern and Midwestern North America.  Different ash species have different distributions within this range</Text>],
         images: {}
-      }, {
+      },
+       {
         title : "Distinguishing different ash species",
         body  : ["Leaf scars, twig shape, and species range can be very helpful in distinguishing ash tree species.",
           "Green ash has a leaf scar below the bud while white ash has a leaf scar that wraps around the bud and is more “U”-shaped.  In addition, white ash leaflets are white-colored on the undersides while green ash leaflets are green or rusty on their undersides. Blue ash can be distinguished by its thick square-shaped twigs which are often winged.  Black ash has a leaf scar similar to green ash but leaflets are directly attached to the petiole without stalks to connect them.  In addition, black ash has a more northern range and is typically found in wet sites.",
@@ -89,7 +89,7 @@ export default Plants = {
       {
         title : "Species",
         images: {},
-        body  : ["Eastern hemlock (Tsuga canadensis)", "Carolina hemlock (Tsuga caroliniana)"],
+        body  : [<Text>Eastern hemlock (<Text style={{fontStyle: "italic"}}>Tsuga canadensis</Text>)</Text>, <Text>Carolina hemlock (<Text style={{fontStyle: "italic"}}>Tsuga caroliniana</Text>)</Text>],
       },
       {
         title : "Overview",
@@ -136,7 +136,7 @@ export default Plants = {
     descriptionCards: [
       {
         title : "Species",
-        body  : ["Quercus alba"],
+        body  : [<Text style={{fontstyle: "italic"}}>Quercus alba </Text>],
         images: {}
       },
       {
@@ -146,7 +146,7 @@ export default Plants = {
       },
       {
         title : "Description",
-        body  : ["American white oak, Quercus alba, is a central component of the central hardwood forests.  Mature white oaks typically grow very large, 80-100 feet high, and have broad, full canopies.  White oaks can live a very long time and some trees have been found that are over 400 years old.  ", "The leaves of white oak have alternate branching and seven to nine rounded lobes.  This is different from red oaks, which typically have pointed lobes with a small hair on each tip.  ", "It is important to note that there are several species in the “white oak” group that share this leaf shape (including swamp white oak and bur oak) and that oaks frequently hybridize, sometimes making it challenging to know which oak species you are looking at."],
+        body  : [<Text>American white oak, <Text style={{fontstyle: "italic"}}>Quercus alba</Text>, is a central component of the central hardwood forests.  Mature white oaks typically grow very large, 80-100 feet high, and have broad, full canopies.  White oaks can live a very long time and some trees have been found that are over 400 years old.  </Text>, "The leaves of white oak have alternate branching and seven to nine rounded lobes.  This is different from red oaks, which typically have pointed lobes with a small hair on each tip.  ", "It is important to note that there are several species in the “white oak” group that share this leaf shape (including swamp white oak and bur oak) and that oaks frequently hybridize, sometimes making it challenging to know which oak species you are looking at."],
         images: {}
       },
       {
