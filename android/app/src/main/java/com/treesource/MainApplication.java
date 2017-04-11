814ed72aec80f7d7f5751e6fdbd480b8100802d4
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-04-11 11:05:50 -0400

    Add app icon and color the splash screen. Remove powered by react

--- android/app/src/main/java/com/treesource/MainApplication.java
+++ android/app/src/main/java/com/treesource/MainApplication.java
@@ -3,6 +3,7 @@ package com.treesource;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.rnfs.RNFSPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNFSPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage(),
--- app/helpers/Colors.js
+++ app/helpers/Colors.js
@@ -5,35 +5,35 @@
  */
 
 const NamedColors = {
-  jungleGreen   : "#2A9D8F",
-  japaneseIndigo: "#264653",
-  lightRedOchre : "#E76F51",
-  sandyBrown    : "#F4A261",
-  hansaYellow   : "#E9C46A",
-  black : '#212121',
-  darkerJungleGreen: "#25897d",
+  jungleGreen      : '#2A9D8F',
+  japaneseIndigo   : '#264653',
+  lightRedOchre    : '#E76F51',
+  sandyBrown       : '#F4A261',
+  hansaYellow      : '#E9C46A',
+  black            : '#212121',
+  darkerJungleGreen: '#25897d'
 }
 
 const Colors = {
-  primary: NamedColors.jungleGreen,
+  primary      : NamedColors.jungleGreen,
   darkerPrimary: NamedColors.darkerJungleGreen,
-  warning: NamedColors.hansaYellow,
-  danger : NamedColors.lightRedOchre,
-  info   : NamedColors.sandyBrown,
-  success: NamedColors.jungleGreen,
-  black : NamedColors.black,
+  warning      : NamedColors.hansaYellow,
+  danger       : NamedColors.lightRedOchre,
+  info         : NamedColors.sandyBrown,
+  success      : NamedColors.jungleGreen,
+  black        : NamedColors.black,
 
-  primaryText: "#ffffff",
-  darkerPrimaryText: "#ffffff",
-  warningText: "#1F2D3D",
-  dangerText: "#ffffff",
-  infoText: "#1F2D3D",
-  successText: "#ffffff",
+  primaryText      : '#ffffff',
+  darkerPrimaryText: '#ffffff',
+  warningText      : '#1F2D3D',
+  dangerText       : '#ffffff',
+  infoText         : '#1F2D3D',
+  successText      : '#ffffff',
 
-  transparentDark: "rgba(0,0,0,.5)",
+  transparentDark: 'rgba(0,0,0,.5)',
 
-  sidebarBackground: "#eeeeee",
-  sidebarText: "#595959"
+  sidebarBackground: '#eeeeee',
+  sidebarText      : '#595959'
 }
 
-export default Colors;
+export default Colors
