0c3b1b668e787847d53e563bfde9cbc54f1931a4
bradfordcondon <bradford.condon@gmail.com>
2017-05-01 10:16:01 -0400

    finalized rename

--- android/app/src/main/java/com/treesnap/MainApplication.java
+++ android/app/src/main/java/com/treesnap/MainApplication.java
@@ -3,6 +3,12 @@ package com.treesnap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.rnfs.RNFSPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +28,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RealmReactPackage(),
+            new VectorIconsPackage(),
+            new ReactMaterialKitPackage(),
+            new MapsPackage(),
+            new RNFSPackage(),
+            new RCTCameraPackage()
       );
     }
   };
