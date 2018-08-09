55b7114d0c0dddce0474428d7c2c2cf5b78339fa
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-08-09 10:28:21 -0400

    Make sure we have a unique id

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
  * @flow
  */
 import React from 'react'
-import {AppRegistry, YellowBox} from 'react-native'
+import { AppRegistry, YellowBox } from 'react-native'
 import TreeSnap from './App'
 
-YellowBox.ignoreWarnings([
-  'Warning: isMounted(...)',
-  'Module RNFetchBlob requires main queue'
-])
+if (__DEV__) {
+  YellowBox.ignoreWarnings([
+    'Warning: isMounted(...)',
+    'Module RNFetchBlob requires main queue'
+  ])
+}
 
 AppRegistry.registerComponent('TreeSnap', () => TreeSnap)
